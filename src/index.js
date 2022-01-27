0ea8f69837d8bcc4adc874b8e10b0eed80e1c87b
bublick <k@lis.im>
2022-01-27 07:51:59 +0200

    release

--- src/index.js
+++ src/index.js
@@ -1,17 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from 'react'
+import ReactDOM from 'react-dom'
+import {
+    titleChanged,
+    taskDeleted,
+    completeTask,
+    loadTasks,
+    getTasks,
+    getTasksLoadingStatus,
+    createNewTask,
+} from './store/task'
+import configureStore from './store/store'
+import { Provider } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { getError } from './store/errors'
+
+const store = configureStore()
+
+const App = (params) => {
+    const state = useSelector(getTasks())
+    const isLoading = useSelector(getTasksLoadingStatus())
+    const error = useSelector(getError())
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(loadTasks())
+    }, [])
+
+    const changeTitle = (taskId) => {
+        dispatch(titleChanged(taskId))
+    }
+    const deleteTask = (taskId) => {
+        dispatch(taskDeleted(taskId))
+    }
+    // const addPostTask = () => {
+    //     dispatch(postRequest())
+    // }
+    if (isLoading) {
+        return <h1>Loading</h1>
+    }
+    if (error) {
+        return <p>{error}</p>
+    }
+
+    return (
+        <>
+            <h1> App</h1>
+            <ul>
+                {state.map((el) => (
+                    <li key={el.id}>
+                        <p>{el.title}</p>
+                        <p> {`Completed: ${el.completed}`}</p>
+                        <button onClick={() => dispatch(completeTask(el.id))}>
+                            Complete
+                        </button>
+                        <button onClick={() => changeTitle(el.id)}>
+                            Change title
+                        </button>
+                        <button onClick={() => deleteTask(el.id)}>
+                            Delete
+                        </button>
+                        <hr />
+                    </li>
+                ))}
+            </ul>
+            <button type='button' onClick={() => dispatch(createNewTask())}>
+                Create Task
+            </button>
+        </>
+    )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
